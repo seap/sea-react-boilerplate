6fa547193792a1326d954dc5b5c2b112d066e58d
seayang <ykudos@gmail.com>
2017-04-05 20:31:59 +0800

    update fetching

--- src/features/common/redux/fetching.js
+++ src/features/common/redux/fetching.js
@@ -18,13 +18,11 @@ export function requestEnd() {
 export function reducer(state, action) {
   switch (action.type) {
     case REQUEST_START:
-    // console.log(1111);
       return {
         ...state,
         fetching: true
       }
     case REQUEST_END:
-    // console.log(222);
       return {
         ...state,
         fetching: false
