a75a36244b536f09e969b4c240ef1f7fb3de5de6
seayang <ykudos@gmail.com>
2017-04-10 20:05:25 +0800

    add dll config

--- webpack/webpack.dll.js
+++ webpack/webpack.dll.js
@@ -1,7 +1,8 @@
 const path = require('path')
 const webpack = require('webpack')
-const webpackConfig = require('./webpack.config')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
+const webpackConfig = require('./webpack.config')
 const vendors = webpackConfig.entry.vendor
 
 module.exports = {
@@ -9,14 +10,26 @@ module.exports = {
     vendor: vendors,
   },
   output: {
-    path: path.resolve(__dirname, './tmp'),
+    path: path.resolve(__dirname, './lib'),
     filename: '[name].[chunkhash:8].js',
     library: '[name]_[chunkhash:8]'
   },
   plugins: [
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('production')
+    }),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, './index.tpl.html'),
+      filename: 'index.html'
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false
+      }
+    }),
     new webpack.DllPlugin({
       context: __dirname,
-      path: path.resolve(__dirname, './tmp/vendor.manifest.json'),
+      path: path.resolve(__dirname, './lib/vendor.manifest.json'),
       name: '[name]_[chunkhash:8]'
     })
   ]
